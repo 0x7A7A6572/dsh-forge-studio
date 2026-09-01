595d0d872a788b73e37c064f9402cd1977136074
fangmingying <zzerx@qq.com>
2026-09-01 09:58:30 +0800

    re-first

--- packages/plugin-notes/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { Context } from '@deepseek-ai/cordis'
-
-export const name = '@forge-studio/dsh-plugin-notes'
-export const inject = []
-
-export function apply(ctx: Context): void {
-  ctx.logger('notes').info('[m0] notes plugin loaded')
-}
